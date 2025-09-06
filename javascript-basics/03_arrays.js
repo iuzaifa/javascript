c041b867ba2efa15b73012ffe7a1a4563b427b08
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-09-07 00:04:07 +0530

    update js

--- javascript-basics/03_arrays.js
+++ javascript-basics/03_arrays.js
@@ -13,17 +13,14 @@ let male = ['Hamza', 'Ali', 'Ahmad', 'Ubaid'];
 let female = ['Saba', 'Afreen'];
 
 let maleFemale = male.concat(female);
-console.log(maleFemale); 
-// [ 'Hamza', 'Ali', 'Ahmad', 'Ubaid', 'Saba', 'Afreen' ]
+console.log(maleFemale); // [ 'Hamza', 'Ali', 'Ahmad', 'Ubaid', 'Saba', 'Afreen' ]
 
 // 2. indexOf() => returns the first index of the given value
 let idxOf = 'Huzaifa';
+console.log(idxOf.indexOf()); // if you call indexOf() with no argument, it searches for 'undefined' // -1 (undefined not found)    
+console.log(idxOf.indexOf('a'));  // finds first occurrence of 'a' ->  4
+console.log(idxOf.indexOf('a', 5)); // starts searching from index 4 // 6
 
-// if you call indexOf() with no argument, it searches for 'undefined'
-console.log(idxOf.indexOf());        // -1 (undefined not found)
 
-// finds first occurrence of 'a'
-console.log(idxOf.indexOf('a'));     // 4
 
-// starts searching from index 4
-console.log(idxOf.indexOf('a', 5));  // 6
+
